5c06f6aaaf92c823a8fc70efa2b1b947b11c911b
Alec Philavong <alecphil@live.com>
2024-05-09 01:20:47 -0500

    added food.js

--- arrayScripts/food.js
+++ arrayScripts/food.js
@@ -1,16 +1,40 @@
 "use strict"
 
-let lunch = [
-    {item: "Steak Fajitas", price: 9.95},
-    {item: "Chips and Guacamole", price: 5.25},
-    {item: "Sweet Tea", price: 2.79}
-    ];
-    
-    let sum = 0;
-    
-    for(let i = 0;i<lunch.length;i++){
 
-        sum +=lunch[i]
+function getMealCost(orders) {
+
+    let sum = 0;
+    let numOrders = orders.length;
+    for (let i = 0; i < numOrders; i++) {
+
+        sum += lunch[i].price;
     }
 
-    console.log(sum)
+    return sum
+
+}
+
+let lunch = [
+    { item: "Steak Fajitas", price: 9.95 },
+    { item: "Chips and Guacamole", price: 5.25 },
+    { item: "Sweet Tea", price: 2.79 }
+];
+
+
+let mealTotal = getMealCost(lunch);
+let tax = mealTotal * (8/100);
+let mealafteraTax = mealTotal + tax;
+let tip = mealafteraTax *(18/100);
+let totalDue = mealafteraTax + tip;
+
+
+
+
+console.log(mealTotal.toFixed(2));
+console.log(tax.toFixed(2));
+console.log(mealafteraTax.toFixed(2));
+console.log(totalDue.toFixed(2));
+
+console.log(`This is the meal total $${mealTotal.toFixed(2)} 
+with a tax of $${tax.toFixed(2)} brings it to $${mealafteraTax.toFixed(2)} 
+with a tip of ${tip.toFixed(2)} it all comes out to $${totalDue.toFixed(2)}. ` )
